55555f1d7452cf8316ef1661075077920ebb3ce7
ROMAN SHESTAKOV <romanshestakov@ROMANs-iMac.home>
2016-03-19 14:32:26 +0000

    add args

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,11 +2,21 @@
 
 cd "$(dirname "${BASH_SOURCE}")";
 
+echo "current dir: $(pwd)"
+
+if [ -z "$1" ]; then
+    DEST=~
+else
+    DEST="$1"
+fi
+
 git pull origin master;
+
+echo "Copying files from $(pwd) to ${DEST}"
 
 function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
-		--exclude "README.md" --exclude "LICENSE-MIT.txt" -avh --no-perms . ~;
+		--exclude "README.md" --exclude "LICENSE-MIT.txt" -avh --no-perms . ${DEST};
 	source ~/.bash_profile;
 }
 
